d3dbd7784ae60998bd9b8fab56902ae19ba8d7fa
buda1994 <hector.torres@unosquare.com>
2017-11-21 14:18:21 -0600

    First Commit

---
+++ WebModule/ImageResizerWebModule.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading.Tasks;
+using Unosquare.Labs.EmbedIO;
+using Unosquare.Labs.EmbedIO.Constants;
+using Unosquare.Swan;
+using Unosquare.Net;
+
+namespace WebModule
+{
+    public class ImageResizerWebModule : WebModuleBase
+    {
+        public ImageResizerWebModule(string folder, int defaultWidth = 500, int defaultHeight = 400)
+        {
+            AddHandler("/api/echo/warsong/", HttpVerbs.Any, (context, ct) =>
+            {
+                return Task.FromResult(ResizeImage(context, folder, defaultWidth, defaultHeight));
+            });
+        }
+
+        private bool ResizeImage(HttpListenerContext context, string image, int defaultWidth, int defaultHeight)
+        {
+            byte[] byteArray;
+
+            var imgIn = new Bitmap(image);
+            double y = imgIn.Height;
+            double x = imgIn.Width;
+
+            double factorWidth = 1;
+            double factorLength = 1;
+
+            if(defaultWidth > 0 && defaultHeight > 0)
+            {
+                factorWidth = defaultWidth / x;
+                factorLength = defaultHeight / y;
+            }
+
+            var outStream = new MemoryStream();
+
+            var imgOut = new Bitmap((int)(x * factorWidth), (int)(y * factorLength));
+
+            imgOut.SetResolution(72, 72);
+
+            var g = Graphics.FromImage(imgOut);
+            g.Clear(Color.White);
+            g.DrawImage(imgIn, new Rectangle(0, 0, (int)(factorWidth * x), (int)(factorLength * y)),
+              new Rectangle(0, 0, (int)x, (int)y), GraphicsUnit.Pixel);
+
+            imgOut.Save(outStream, getImageFormat(image));
+            byteArray = outStream.ToArray();
+            
+            context.Response.OutputStream.Write(byteArray, 0, byteArray.Length);
+            
+            return true;
+        }
+
+        ImageFormat getImageFormat(String path)
+        {
+            switch(Path.GetExtension(path))
+            {
+                case ".bmp":
+                return ImageFormat.Bmp;
+                case ".gif":
+                return ImageFormat.Gif;
+                case ".jpg":
+                return ImageFormat.Jpeg;
+                case ".png":
+                return ImageFormat.Png;
+                default:
+                break;
+            }
+            return ImageFormat.Jpeg;
+        }
+
+        public override string Name => nameof(ImageResizerWebModule).Humanize();
+    }
+
+}
---
+++ WebModule/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Unosquare.Labs.EmbedIO;
+using Unosquare.Labs.EmbedIO.Constants;
+using Unosquare.Labs.EmbedIO.Modules;
+using Unosquare.Net;
+
+namespace WebModule
+{
+    class Program
+    {
+        public static string HtmlRootPath
+        {
+            get
+            {
+                var assemblyPath = Path.GetDirectoryName(typeof(Program).GetTypeInfo().Assembly.Location);
+
+                return Path.Combine(Directory.GetParent(assemblyPath).Parent.Parent.FullName, "Warsong.png");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var url = "http://localhost:9696/";
+
+            using(var server = new WebServer(url, RoutingStrategy.Regex))
+            {
+                server.RegisterModule(new ImageResizerWebModule(HtmlRootPath,0,0));
+                
+                server.RunAsync();
+
+                var browser = new System.Diagnostics.Process()
+                {
+                    StartInfo = new System.Diagnostics.ProcessStartInfo(url + "api/echo/Warsong") { UseShellExecute = true }
+                };
+                browser.Start();
+
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
